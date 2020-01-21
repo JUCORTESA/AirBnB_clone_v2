83cfbd49c4e9769bf6ec5178a45599758a8d09ff
jucortesa <984@holbertonschool.com>
2020-01-21 18:56:45 +0000

    [fix] underscore

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,8 @@ def hbnb():
 @app.route('/c/<text>/', strict_slashes=False)
 def c(text):
     """c route"""
-    return("C {}".format(text))
+    new_text = text.replace("_", " ")
+    return("C {}".format(new_text))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
