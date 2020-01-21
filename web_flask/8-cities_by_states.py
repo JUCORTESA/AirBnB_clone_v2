59cdd884ef0bbcbaa1e566c3fd3c33428d28b48b
jucortesa <984@holbertonschool.com>
2020-01-21 21:53:12 +0000

    [update] all tasks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,16 +1,62 @@
 #!/usr/bin/python3
 """
-a script that starts a Flask web application
+A python script that starts a Flask web application
 """
-from models import storage, State, City
 from flask import Flask, render_template
+from models import storage, State, City
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 a_dict = storage.all('State').values()
 b_dict = storage.all('City').values()
 
 
-@app.route('/states_list')
+@app.route('/', strict_slashes=False)
+def index():
+    """index page"""
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route"""
+    return("HBNB")
+
+
+@app.route('/c/<text>/', strict_slashes=False)
+def c(text):
+    """c route"""
+    return("C {}".format(text))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>/', strict_slashes=False)
+def python(text="is cool"):
+    """python route"""
+    if text != "is cool":
+        new_text = text.replace("_", " ")
+        return("Python {}".format(new_text))
+    return("Python {}".format(text))
+
+
+@app.route('/number/<int:n>/', strict_slashes=False)
+def number(n):
+    """number route"""
+    return("{} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>/', strict_slashes=False)
+def number_template(n=None):
+    """number_template route"""
+    return(render_template("5-number.html", n=n))
+
+
+@app.route('/number_odd_or_even/<int:n>/', strict_slashes=False)
+def odd_or_even(n=None):
+    """number_odd_or_even route"""
+    return(render_template("6-number_odd_or_even.html", n=n))
+
+
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     """
     Function that returns a list of states
@@ -18,7 +64,7 @@ def states_list():
     return render_template('7-states_list.html', my_dict=a_dict)
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
     return render_template('8-cities_by_states.html',
                            my_dict=a_dict, my_city=b_dict)
